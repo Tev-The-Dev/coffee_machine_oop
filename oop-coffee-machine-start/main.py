0e5890c08dc6018a7fb966d03e53575ffea82a6b
Tevyn <tevyn.payne@gmail.com>
2025-03-03 17:21:38 -0600

    completing step 1: pring the menu and ask customer to choose an option

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -2,4 +2,13 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+making_coffee = True
+
+menu = Menu()
+
+while making_coffee:
+    print (f"What would you like? {menu.get_items()}: ")
+    choice = str(input())
+    if choice not in menu.get_items():
+        making_coffee = False
 
