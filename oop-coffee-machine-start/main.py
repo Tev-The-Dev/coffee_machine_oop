bfda334ba38d18b92699bb30d2d3c570589a6e70
Tevyn <tevyn.payne@gmail.com>
2025-03-03 18:07:43 -0600

    making adjustments to display the report

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -2,13 +2,31 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-making_coffee = True
 
 menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 
+making_coffee = True
 while making_coffee:
     print (f"What would you like? {menu.get_items()}: ")
     choice = str(input())
-    if choice not in menu.get_items():
+    if choice == "off":
+        in_maintenance_mode = True
+        print("Entering maintenance mode")
+        while in_maintenance_mode:
+            maintain = str(input("What would you like to do?"))
+            if maintain == "report":
+                coffee_maker.report()
+                money_machine.report()
+            else:
+                print("Going back to regular usage")
+                in_maintenance_mode = False
+    elif choice in menu.get_items():
+        order = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(order):
+            if money_machine.make_payment(order.cost):
+                coffee_maker.make_coffee(order)
+    else:
+        print("Thank you for making coffee with us!")
         making_coffee = False
-
